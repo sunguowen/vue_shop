8efc6f45f28d568f40e795a4ee23c431c425707d
sunguowen <m18945097312@163.com>
2020-03-03 23:55:03 +0800

    finish login module

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': 4
+    'Missing space before function parentheses': 0
   }
 }
