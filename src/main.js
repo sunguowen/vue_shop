4b63bd8136989dde12fa77bb06ff998e538d508e
Guowen Sun <m18945097312.163.com>
2020-03-10 17:34:47 +0800

    addDescription

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import TreeTable from 'vue-table-with-tree-grid'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 请求拦截器
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // console.log(config)
