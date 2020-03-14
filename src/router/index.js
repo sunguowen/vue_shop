e8f698d3e91bb7db4a426b1c0d3b017271c070a7
Guowen Sun <m18945097312.163.com>
2020-03-14 14:00:08 +0800

    finish reports module

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import Goods from '../components/goods/goods_list.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/order_list.vue'
+import Report from '../components/report/reports.vue'
 
 Vue.use(VueRouter)
 
@@ -30,7 +31,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: Goods },
       { path: '/goods/add', component: Add },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   }
 ]
