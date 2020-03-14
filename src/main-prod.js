75b50b6eeb1065783d6a90de987cce73696401cf
Guowen Sun <m18945097312.163.com>
2020-03-14 21:41:11 +0800

    项目代码优化

---
+++ src/main-prod.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import NProgress from 'nprogress'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 请求拦截器
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // console.log(config)
+  return config
+})
+// 在相应拦截器中隐藏进度条
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+Vue.filter('formatDate', function(originVal) {
+  const date = new Date(originVal)
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1 + '').padStart(2, '0')
+  const day = (date.getDate() + '').padStart(2, '0')
+  const hour = (date.getHours() + '').padStart(2, '0')
+  const minutes = (date.getMinutes() + '').padStart(2, '0')
+  const second = (date.getSeconds() + '').padStart(2, '0')
+  return `${year}-${month}-${day} ${hour}:${minutes}:${second}`
+})
+
+Vue.prototype.$http = axios
+Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+// 将富文本编辑器注册为全局组件
+Vue.use(VueQuillEditor)
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,58 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Roles.vue'
-import Cate from '../components/goods/Cate.vue'
-import Params from '../components/goods/Params.vue'
-import Goods from '../components/goods/goods_list.vue'
-import Add from '../components/goods/Add.vue'
-import Order from '../components/order/order_list.vue'
-import Report from '../components/report/reports.vue'
+// import Login from '../components/login.vue'
+const Login = () =>
+  import(/* webpackChunkName: "login_home_welcome" */ '../components/login.vue')
+// import Home from '../components/Home.vue'
+const Home = () =>
+  import(/* webpackChunkName: "login_home_welcome" */ '../components/Home.vue')
+// import Welcome from '../components/Welcome.vue'
+const Welcome = () =>
+  import(
+    /* webpackChunkName: "login_home_welcome" */ '../components/Welcome.vue'
+  )
+// import Users from '../components/user/Users.vue'
+const Users = () =>
+  import(
+    /* webpackChunkName: "users_rights_roles" */ '../components/user/Users.vue'
+  )
+// import Rights from '../components/power/Rights.vue'
+const Rights = () =>
+  import(
+    /* webpackChunkName: "users_rights_roles" */ '../components/power/Rights.vue'
+  )
+// import Roles from '../components/power/Roles.vue'
+const Roles = () =>
+  import(
+    /* webpackChunkName: "users_rights_roles" */ '../components/power/Roles.vue'
+  )
 
+// import Cate from '../components/goods/Cate.vue'
+const Cate = () =>
+  import(/* webpackChunkName: "cate_params" */ '../components/goods/Cate.vue')
+// import Params from '../components/goods/Params.vue'
+const Params = () =>
+  import(/* webpackChunkName: "cate_params" */ '../components/goods/Params.vue')
+
+// import Goods from '../components/goods/goods_list.vue'
+const Goods = () =>
+  import(
+    /* webpackChunkName: "goods_add" */ '../components/goods/goods_list.vue'
+  )
+// import Add from '../components/goods/Add.vue'
+const Add = () =>
+  import(/* webpackChunkName: "goods_add" */ '../components/goods/Add.vue')
+
+// import Order from '../components/order/order_list.vue'
+const Order = () =>
+  import(
+    /* webpackChunkName: "order_report" */ '../components/order/order_list.vue'
+  )
+// import Report from '../components/report/reports.vue'
+const Report = () =>
+  import(
+    /* webpackChunkName: "order_report" */ '../components/report/reports.vue'
+  )
 Vue.use(VueRouter)
 
 const routes = [
